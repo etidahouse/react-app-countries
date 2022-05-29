59959d58e6d7236afa10890a08c78c28d2db9e21
Étienne <e-begon@hotmail.fr>
2022-05-29 16:23:41 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Logo from "../components/Logo"
+import Navigation from "../components/Navigation"
+
+const About = () => {
+  return (
+    <div>
+        <Logo />
+        <Navigation />
+      <h1>A propos</h1>
+      <p>
+          lorem ipsum
+      </p>
+      <br/>
+      <p>
+          lorem ipsum
+      </p>
+    </div>
+  )
+}
+
+export default About
